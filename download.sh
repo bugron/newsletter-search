8fd0df20ffa69928bf9e70e7bdbb0738861cadde
Arsen Melikyan <bugron@mail.ru>
2024-09-27 09:18:17 +0400

    Update download script

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in {1..324}
+for i in {110..202}
 do
     echo "Downloading file for number $i"
-    curl -o "$i.html" "https://bytes.dev/archives/$i"
+    curl -o "$i.html" "https://thisweekinreact.com/newsletter/$i"
     # Add a small delay to avoid overwhelming the server
     sleep $((RANDOM % 3 + 3))
 done
